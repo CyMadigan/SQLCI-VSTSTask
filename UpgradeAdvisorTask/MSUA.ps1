b474f946c2379ae5ada33e87245935abbbd8a9d1
David Pond <davidpond@gmail.com>
2016-04-03 14:35:50 +0100

    Tidy output of MSUA script

--- UpgradeAdvisorTask/MSUA.ps1
+++ UpgradeAdvisorTask/MSUA.ps1
@@ -12,12 +12,11 @@ $msuaZipFile = '.\msua.7z'
 $sevenZipExe = join-path '.\7Z' "7z.exe"
 Write-host $sevenZipExe
 set-alias sz $sevenZipExe
-sz x $msuaZipFile
+sz x $msuaZipFile | Out-Null
 $msuaDirectory = '.\Microsoft SQL Server 2016 Upgrade Advisor'
 Write-host "Completed extracting MSUA"
 
 #Run
-Write-host "Running MSUA on $server\$database using auth=$authMethod user=$username and password=$password"
 $msuaExe = join-path $msuaDirectory "SqlAdvisorCmd.exe"
 set-alias msua $msuaExe
 if($authMethod -eq "sqlServerAuth")
@@ -28,7 +27,7 @@ else
 {
 	$connectionString = "Data Source=$server;Initial Catalog=$database;Integrated Security=SSPI;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"
 }
-Write-host "Connection string = $connectionString"
+Write-host "Running MSUA with connection string = $connectionString"
 Write-host "Testing 'upgrade' scenario"
 msua /FolderPath:"$Env:SYSTEM_DEFAULTWORKINGDIRECTORY" /Scenario:Upgrade /ConnectionStrings:"$connectionString"
 Write-host "Testing 'stretch DB' scenario"

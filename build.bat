07b1a36ade2c5089894c1c5c1dac1b54992963ee
David Pond <davidpond@gmail.com>
2016-04-03 14:24:35 +0100

    Tidy up build steps including separate out Tasks different file copy steps.

--- build.bat
+++ build.bat
@@ -3,7 +3,10 @@
 call npm install
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-call powershell -NoProfile -NonInteractive -NoLogo -ExecutionPolicy unrestricted -file moveFiles.ps1
+call powershell -NoProfile -NonInteractive -NoLogo -ExecutionPolicy unrestricted -file build_CopyAutomationSuiteFiles.ps1
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+call powershell -NoProfile -NonInteractive -NoLogo -ExecutionPolicy unrestricted -file build_CopyUpgradeAdvisorFiles.ps1
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 call .\node_modules\.bin\vset.cmd package -m extension-manifest.json -o .\Packages
--- build_CopyUpgradeAdvisorFiles.ps1
+++ build_CopyUpgradeAdvisorFiles.ps1
@@ -1,29 +1,5 @@
 $ErrorActionPreference = 'Stop'
 
-If (!$env:DLMAS_HOME)
-{
-    Write-Host '** Error: DLM Automation Suite not installed. Exiting **'
-    Exit 1
-}
-
-$sqlCiDirectory            = $env:DLMAS_HOME + 'SQLCI\'
-$sqlCompareDirectory       = $env:DLMAS_HOME + 'SC\'
-
-@('.\TheTask\SC', '.\TheTask\SDG', '.\TheTask\sqlCI') | % {
-	if (Test-Path $_) {
-		Remove-Item $_ -Recurse
-	}
-}
-
-Copy-Item $sqlCiDirectory            .\TheTask\sqlCI  -recurse -force
-Copy-Item $sqlCompareDirectory       .\TheTask\SC     -recurse -force
-
-# To get around bug with VSO Marketplace that you can't have both an .EXT and an .ext 
-$DocCommentsFile = '.\TheTask\sqlCI\sqlCI.XML'
-if (Test-Path $DocCommentsFile) {
-	Remove-Item $DocCommentsFile -Recurse
-}
-
 #We need 7Zip to compress MSUA to get under 20Mb limit :-(
 If (!$env:SevenZip_HOME)
 {
